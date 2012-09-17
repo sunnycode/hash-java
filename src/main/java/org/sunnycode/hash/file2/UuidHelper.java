2b438812a8e1ecc42a99363f27b6c01b5e51a73a
Sunny Gleason <sunny.gleason@gmail.com>
2012-09-17 08:56:42 -0400

    fix uuid to be 16-byte instead of 8-byte

--- src/main/java/org/sunnycode/hash/file2/UuidHelper.java
+++ src/main/java/org/sunnycode/hash/file2/UuidHelper.java
@@ -5,7 +5,7 @@ import java.util.UUID;
 
 public class UuidHelper {
     public static byte[] uuidToBytes(UUID uuid) {
-        ByteBuffer buf = ByteBuffer.allocate(8);
+        ByteBuffer buf = ByteBuffer.allocate(16);
 
         buf.putLong(uuid.getMostSignificantBits());
         buf.putLong(uuid.getLeastSignificantBits());
